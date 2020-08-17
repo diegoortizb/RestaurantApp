ec1c7095dad5ce43fdcdf86a4df3da5b913248c6
Diego Ortiz <ortizdiego@comcast.net>
2020-08-16 20:41:12 -0500

    adding a table java file

--- src/main/java/restaurant/project/com/table/Table.java
+++ src/main/java/restaurant/project/com/table/Table.java
@@ -2,4 +2,29 @@ package restaurant.project.com.table;
 
 public class Table {
 
+    //position x
+    private int posX;
+    //position y
+    private int posY;
+
+    public Table(int posX, int posY) {
+        this.posX = posX;
+        this.posY = posY;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public void setPosY(int posY) {
+        this.posY = posY;
+    }
 }
