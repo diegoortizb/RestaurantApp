9f94b6ec4f972131c49ffdd1ab0a73945b1a19de
J-Erwin <johnny.composer.man@gmail.com>
2020-08-16 21:12:09 -0500

    Added frontend react framework

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Not setup yet...Check back later!</header>
+    </div>
+  );
+}
