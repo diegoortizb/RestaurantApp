02ecc96b7c8fc75598f043d26048f0c304c25380
Diego Ortiz <ortizdiego@comcast.net>
2020-08-17 23:57:55 -0500

    removing imports

--- src/main/java/restaurant/project/controllers/HomeController.java
+++ src/main/java/restaurant/project/controllers/HomeController.java
@@ -1,7 +1,6 @@
 package restaurant.project.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
--- src/main/java/restaurant/project/controllers/SchedulesController.java
+++ src/main/java/restaurant/project/controllers/SchedulesController.java
@@ -1,8 +1,6 @@
 package restaurant.project.controllers;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
