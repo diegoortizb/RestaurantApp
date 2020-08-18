e21581412f3a7ba015a73b2fa4df2b4c9a343788
Diego Ortiz <ortizdiego@comcast.net>
2020-08-17 23:50:33 -0500

    REST controller for home

--- src/main/java/restaurant/project/controllers/HomeController.java
+++ src/main/java/restaurant/project/controllers/HomeController.java
@@ -1,13 +1,20 @@
 package restaurant.project.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String index() {
-        return "home";
+    @GetMapping("/home")
+    @ResponseBody
+    public ModelAndView index () {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("home");
+        return modelAndView;
     }
 }
